8db0e82996ee69415e23ce7cb825961093fd0196
christian-stephen <chrisj.stephen2@gmail.com>
2017-06-07 16:32:29 -0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='apriori',
+    version='1.0',
+    description='An implementation of the Apriori algorithm for association rule mining',
+    long_description=long_description,
+    author='Christian Stephen',
+    author_email='chrisj.stephen2@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'Topic :: Database',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    py_modules=['apriori', 'export_result', 'generate_candidates', 'generate_rules',
+                'load_database', 'prune_itemsets']
+)
