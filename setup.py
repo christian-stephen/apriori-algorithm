1cc2526524d3eb9deeee3eca729a3a8472fcdb87
christian-stephen <chrisj.stephen2@gmail.com>
2017-06-08 09:00:55 -0300

    Changed single quotes to double quotes
    It's more consistent

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='apriori',
-    version='1.0',
-    description='An implementation of the Apriori algorithm for association rule mining',
+    name="apriori",
+    version="1.0",
+    description="An implementation of the Apriori algorithm for association rule mining",
     long_description=long_description,
-    author='Christian Stephen',
-    author_email='chrisj.stephen2@gmail.com',
-    license='MIT',
-    py_modules=['apriori', 'export_result', 'generate_candidates', 'generate_rules',
-                'load_database', 'prune_itemsets']
+    author="Christian Stephen",
+    author_email="chrisj.stephen2@gmail.com",
+    license="MIT",
+    py_modules=["apriori", "export_result", "generate_candidates", "generate_rules",
+                "load_database", "prune_itemsets"]
 )
