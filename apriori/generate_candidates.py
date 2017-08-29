5408f0e4cb60533ce9620a2878d36e20494bac56
Christian Stephen <chrisj.stephen2@gmail.com>
2017-08-29 00:37:44 -0500

    Removed unnecessary variable

--- apriori/generate_candidates.py
+++ apriori/generate_candidates.py
@@ -13,6 +13,5 @@ def generate_candidate_itemsets(itemsets, itemset_sups):
             # Find the symmetric difference to ensure only the first k-2 items match.
             itemlist_diff = list(itemset1 ^ itemset2)
             if len(itemlist_diff) == 2 and itemlist_diff[0][0] != itemlist_diff[1][0]: 
-                candidate_itemset = itemset1 | itemset2
-                candidate_itemsets.add(candidate_itemset)
+                candidate_itemsets.add(itemset1 | itemset2)
     return candidate_itemsets
